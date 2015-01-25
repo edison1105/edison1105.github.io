1d8d7ec9f1906d51608678607edb165010c73a2a
edison1105 <daiwei521@126.com>
2015-01-25 18:52:13 +0800

    Site updated: 2015-01-25 18:52:13

--- js/main.js
+++ js/main.js
@@ -119,16 +119,6 @@ var Main = (function(){
 $(Main.init());
 
 
-(function() {
-    var c = document.createElement('script'); 
-    c.type = 'text/javascript';
-    c.async = true;
-    c.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'www.clicki.cn/boot/51686';
-    var h = document.getElementsByTagName('script')[0];
-    h.parentNode.insertBefore(c, h);
-})();
-
-
 
 
 
